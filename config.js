456b78e843e319f6ad524c7bf1d998196d30b5be
ASTROPLAYS2 <hbwabot0@gmail.com>
2025-01-19 21:33:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
-    PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Ibrahim Adams",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " Ibrahim Adams",              
+    PREFIXE: process.env.PREFIX || "±",
+    OWNER_NAME: process.env.OWNER_NAME || "۝AstrO۝",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || " 917085345102",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
-    BOT : process.env.BOT_NAME || 'BMW_MD',
+    BOT : process.env.BOT_NAME || '۝AstrO۝',
     URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/h2ydge.jpg',
     MODE: process.env.PUBLIC_MODE || "yes",
     PM_PERMIT: process.env.PM_PERMIT || 'yes',
